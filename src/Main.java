443a15441cd1f0b54701ab9cf61c8744a99a5cc0
Gabriel Dutra <gabriel-dutralima@outlook.com>
2025-01-14 18:25:19 -0300

    Refatorada a estrutura do código para melhorar a modularização e facilitar a manutenção.

--- src/Main.java
+++ src/Main.java
@@ -22,144 +22,31 @@ public class Main {
         Collections.sort(tarefas);
         int opcao = -1;
 
-        while (opcao != 0) {
-            System.out.println("\n === Gerenciador de Tarefas ===");
-            System.out.println("1. Adicionar Tarefa");
-            System.out.println("2. Listar Tarefas");
-            System.out.println("3. Remover Tarefa");
-            System.out.println("4. Marcar como Concluída");
-            System.out.println("5. Filtrar Tarefas por Status");
-            System.out.println("0. Sair");
-            System.out.println("Escolha uma opção: ");
-
+        while (opcao !=0 ) {
+            exibirMenu();
             opcao = scanner.nextInt();
-            scanner.nextLine();
-            //=====================1===============================
-            if (opcao == 1) {
-                System.out.println("--- Digite o Titulo da tarefa: ---");
-                String titulo = scanner.nextLine();
-
-                System.out.println("Digite a descrição da tarefa: ");
-                String descricao = scanner.nextLine();
+            scanner.nextLine(); // Consumi o enter extra
 
 
-                System.out.println("Digite a data de vencimento (dd/MM/yyyy): ");
-                String dataEntrada = scanner.nextLine();
-                LocalDate dataVencimento;
-
-                try {
-                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-                    dataVencimento = LocalDate.parse(dataEntrada, formatter);
-                } catch (Exception e) {
-                    System.out.println("Data invalida! A Tarefa não foi criada.");
-                    continue; // Volta para o menu principal
+            switch (opcao) {
+                case 1 -> adicionarTarefa(scanner, tarefas);
+                case 2 -> listarTarefas(tarefas);
+                case 3 -> removerTarefa(scanner, tarefas);
+                case 4 -> marcarComoConcluida(scanner, tarefas);
+                case 5 -> filtrarTarefas(scanner, tarefas);
+                case 0 -> {
+                    salvarTarefas(tarefas);
+                    System.out.println("--- Saindo do Gerenciador de Tarefas ---");
                 }
-                Tarefa novaTarefa = new Tarefa(titulo, descricao, dataVencimento);
-                tarefas.add(novaTarefa);
-
-                System.out.println("Tarefa Adicionada!!");
-
-                //=====================2===============================
-            } else if (opcao == 2) {
-                System.out.println("\n--- Listando Tarefas! ---");
-                if (tarefas.isEmpty()) {
-                    System.out.println("Nenhuma tarefa registrada.");
-                } else {
-                    Collections.sort(tarefas);
-                    for (int i = 0; i < tarefas.size(); i++) {
-                        System.out.println((i + 1) + ". " + tarefas.get(i));
-                    }
-                }
-
-                //=====================3===============================
-            } else if (opcao == 3) {
-                System.out.println("\n--- Remover Tarefa! ---");
-                if (tarefas.isEmpty()) {
-                    System.out.println("Nenhuma tarefa para remover.");
-                } else {  // Exibe as tarefas
-                    System.out.println("Tarefas disponiveis: ");
-                    for (int i = 0; i < tarefas.size(); i++) {
-                        System.out.println((i + 1));
-                    }
-
-                    // Solicita o numero de tarefas
-                    System.out.println("Digite o numero da tarefa que deseja remover: ");
-                    int numero = scanner.nextInt();
-
-                    // valida a entrada do usuario
-                    if (numero > 0 && numero <= tarefas.size()) {
-                        tarefas.remove(numero - 1); // Remove a tarefa
-                        System.out.println("Tarefa removida!!");
-
-                    } else {
-                        System.out.println("Numero Invalido !!");
-                    }
-                }
-
-                //=====================4===============================
-            } else if (opcao == 4) {
-                System.out.println("--- Marcar Tarefa como Concluida ---");
-                if (tarefas.isEmpty()) {
-                    System.out.println("Tarefa não encontrada");
-                } else {
-                    System.out.println("Tarefas disponiveis:");
-                    for (int i = 0; i < tarefas.size(); i++) {
-                        System.out.println((i + 1) + ". " + tarefas.get(i));
-
-                    }
-
-                    System.out.println("Escolha a tarefa para marcar como conclida: ");
-                    int numero = scanner.nextInt();
-
-                    if (numero > 0 && numero <= tarefas.size()) {
-                        tarefas.get(numero - 1).marcarConcluido();
-                        System.out.println("Tarefa Concluída!");
-                    } else {
-                        System.out.println("Numero inválido. Por favor, tente novamente.");
-
-                    }
-                }
-
-            } else if (opcao == 5) {
-                System.out.println("\n--- Filtrar Tarefas ---");
-                System.out.println("1. Mostrar apenas tarefas pendentes");
-                System.out.println("2. Mostrar apenas tarefas concluidas");
-                System.out.print("Escolha uma opção: ");
-
-                int filtro = scanner.nextInt();
-                //scanner.nextInt();  usado para consumir a quebra de linha
-
-                if (filtro == 1) {
-                    System.out.println("\n--- Tarefas Pendentes ---");
-                    for (Tarefa tarefa : tarefas) {
-                        if (!tarefa.isConcluido()) {
-                            System.out.println(tarefa);
-                        }
-                    }
-                } else if (filtro == 2) {
-                    System.out.println("\n--- Tarefas Concluídas ---");
-                    for (Tarefa tarefa : tarefas) {
-                        if (!tarefa.isConcluido()) {
-                            System.out.println(tarefa);
-                        }
-                    }
-
-                }else {
-                    System.out.println("Opção inválida!");
-                }
-
-            }else if (opcao == 0) {
-                salvarTarefas(tarefas);
-                System.out.println("--- Saindo do Gerenciador de Tarefas ---");
-            } else {
-                System.out.println("Opção invalida !!!");
+                default -> System.out.println("Opção inválida!!");
             }
 
+        }
+        scanner.close();
 
         }
 
-            scanner.close();
-    }
+    //}
 
     public static void salvarTarefas(ArrayList<Tarefa> tarefas) {
         try (FileWriter writer = new FileWriter("tarefas.txt")) {
@@ -173,7 +60,7 @@ public class Main {
     }
 
     public static void carregarTarefas(ArrayList<Tarefa> tarefas) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("tarefas.txt"))){
+        try (BufferedReader reader = new BufferedReader(new FileReader("tarefas.txt"))) {
             String linha;
             while ((linha = reader.readLine()) != null) {
                 String[] partes = linha.split(";");
@@ -197,7 +84,7 @@ public class Main {
                     System.out.println("Erro ao ler a data de Vencimento: " + partes[3]);
                     continue; // Ignora esta linha e continua para a proxima
                 }
-                Tarefa tarefa = new Tarefa(titulo,  descricao, dataVencimento);
+                Tarefa tarefa = new Tarefa(titulo, descricao, dataVencimento);
                 if (concluida) {
                     tarefa.marcarConcluido();
                 }
@@ -206,6 +93,130 @@ public class Main {
             System.out.println("Tarefa carregadas com sucesso!");
         } catch (IOException e) {
             System.out.println("Erro ao carregar tarefas: " + e.getMessage());
+        }
+    }
+
+    public static void exibirMenu() {
+        System.out.println("\n === Gerenciador de Tarefas ===");
+        System.out.println("1. Adicionar Tarefa");
+        System.out.println("2. Listar Tarefas");
+        System.out.println("3. Remover Tarefa");
+        System.out.println("4. Marcar como Concluída");
+        System.out.println("5. Filtrar Tarefas por Status");
+        System.out.println("0. Sair");
+        System.out.println("Escolha uma opção: ");
+    }
+
+    public static void adicionarTarefa(Scanner scanner, ArrayList<Tarefa> tarefas) {
+        System.out.println("--- Digite o Titulo da tarefa: ---");
+        String titulo = scanner.nextLine();
+
+        System.out.println("Digite a descrição da tarefa: ");
+        String descricao = scanner.nextLine();
+
+
+        System.out.println("Digite a data de vencimento (dd/MM/yyyy): ");
+        String dataEntrada = scanner.nextLine();
+        LocalDate dataVencimento;
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            dataVencimento = LocalDate.parse(dataEntrada, formatter);
+            Tarefa novaTarefa = new Tarefa(titulo, descricao, dataVencimento);
+            tarefas.add(novaTarefa);
+            System.out.println("Tarefa Adicionada!");
+        } catch (Exception e) {
+            System.out.println("Data inválida! A tarefa não foi salva.");
+        }
+    }
+
+    public static void listarTarefas(ArrayList<Tarefa> tarefas) {
+        System.out.println("\n--- Listando Tarefas! ---");
+        if (tarefas.isEmpty()) {
+            System.out.println("Nenhuma tarefa registrada.");
+        } else {
+            Collections.sort(tarefas);
+            for (int i = 0; i < tarefas.size(); i++) {
+                System.out.println((i + 1) + ". " + tarefas.get(i));
+            }
+        }
+    }
+
+    public static void removerTarefa(Scanner scanner, ArrayList<Tarefa> tarefas) {
+        System.out.println("\n--- Remover Tarefa! ---");
+        if (tarefas.isEmpty()) {
+            System.out.println("Nenhuma tarefa para remover.");
+        } else {  // Exibe as tarefas
+            System.out.println("Tarefas disponiveis: ");
+            for (int i = 0; i < tarefas.size(); i++) {
+                System.out.println((i + 1));
+            }
+
+            // Solicita o numero de tarefas
+            System.out.println("Digite o numero da tarefa que deseja remover: ");
+            int numero = scanner.nextInt();
+
+            // valida a entrada do usuario
+            if (numero > 0 && numero <= tarefas.size()) {
+                tarefas.remove(numero - 1); // Remove a tarefa
+                System.out.println("Tarefa removida!!");
+
+            } else {
+                System.out.println("Numero Invalido !!");
+            }
+        }
+    }
+
+    public static void marcarComoConcluida(Scanner scanner, ArrayList<Tarefa> tarefas) {
+        System.out.println("--- Marcar Tarefa como Concluida ---");
+        if (tarefas.isEmpty()) {
+            System.out.println("Tarefa não encontrada");
+        } else {
+            System.out.println("Tarefas disponiveis:");
+            for (int i = 0; i < tarefas.size(); i++) {
+                System.out.println((i + 1) + ". " + tarefas.get(i));
+
+            }
+
+            System.out.println("Escolha a tarefa para marcar como conclida: ");
+            int numero = scanner.nextInt();
+
+            if (numero > 0 && numero <= tarefas.size()) {
+                tarefas.get(numero - 1).marcarConcluido();
+                System.out.println("Tarefa Concluída!");
+            } else {
+                System.out.println("Numero inválido. Por favor, tente novamente.");
+
+            }
+        }
+    }
+
+    public static void filtrarTarefas(Scanner scanner, ArrayList<Tarefa> tarefas) {
+        System.out.println("\n--- Filtrar Tarefas ---");
+        System.out.println("1. Mostrar apenas tarefas pendentes");
+        System.out.println("2. Mostrar apenas tarefas concluidas");
+        System.out.print("Escolha uma opção: ");
+
+        int filtro = scanner.nextInt();
+        scanner.nextLine(); // usado para consumir a quebra de linha
+
+        if (filtro == 1) {
+            System.out.println("\n--- Tarefas Pendentes ---");
+            for (Tarefa tarefa : tarefas) {
+                if (!tarefa.isConcluido()) {
+                    System.out.println(tarefa);
+                }
+            }
+        } else if (filtro == 2) {
+            System.out.println("\n--- Tarefas Concluídas ---");
+            for (Tarefa tarefa : tarefas) {
+                if (tarefa.isConcluido()) {
+                    System.out.println(tarefa);
+                }
+            }
+
+        } else {
+            System.out.println("Opção inválida!");
         }
     }
 }
